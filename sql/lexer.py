10c58c3816e0079fba65b00a8b7e2df73590e978
juntaoduan <atomduan@gmail.com>
2020-08-06 18:22:05 +0800

    add start condition

--- sql/lexer.py
+++ sql/lexer.py
@@ -1,20 +1,71 @@
 #!/usr/local/bin/python3
 
+#https://www.dabeaz.com/ply/ply.html#ply_nn21
+
 import sys
 import lex
 
 tokens = (
-    'CMP',
+    'COMPARISON',
     'INTNUM',
     'STRING',
     'OTHER',
+    'SELECT',
+    'FROM',
+    'WHERE',
+    'LIKE',
+    'AS',
 )
 
-t_OTHER     = r'.'
+states = (
+    ('strsc','exclusive'),
+)
 
-def t_CMP(t):
+t_OTHER         = r'.'
+t_ignore        = ' \t'
+t_strsc_ignore  = r''
+
+def t_begin_strsc(t):
+    r'\"'
+    t.lexer.begin('strsc')
+
+def t_strsc_STRCTNT(t):
+    r'[^\"]+'
+    t.type = 'STRING'
+    return t
+
+def t_strsc_end(t):
+    r'\"'
+    t.lexer.begin('INITIAL')
+
+def t_SELECT(t):
+    r'(?i)SELECT'
+    t.type = 'SELECT'
+    return t
+
+def t_FROM(t):
+    r'(?i)FROM'
+    t.type = 'FROM'
+    return t
+
+def t_WHERE(t):
+    r'(?i)WHERE'
+    t.type = 'WHERE'
+    return t
+
+def t_LIKE(t):
+    r'(?i)LIKE'
+    t.type = 'LIKE'
+    return t
+
+def t_AS(t):
+    r'(?i)AS'
+    t.type = 'AS'
+    return t
+
+def t_COMPARISON(t):
     r'(=|<>|<|>|<=|>=)'
-    t.type = 'CMP'
+    t.type = 'COMPARISON'
     return t
 
 def t_INTNUM(t):
@@ -28,13 +79,11 @@ def t_STRING(t):
     t.type = 'STRING'
     return t
 
-t_ignore  = ' \t'
-
-def t_newline(t):
+def t_ANY_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-def t_error(t):
+def t_ANY_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
