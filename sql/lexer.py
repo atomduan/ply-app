b1594fc9447990b0e27c5377af372d7233bb99cb
juntaoduan <atomduan@gmail.com>
2020-08-06 17:09:55 +0800

    add

--- sql/lexer.py
+++ sql/lexer.py
@@ -3,47 +3,40 @@
 import sys
 import lex
 
-
-# List of token names.   This is always required
 tokens = (
-    'NUMBER',
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
-    'LPAREN',
-    'RPAREN',
+    'CMP',
+    'INTNUM',
+    'STRING',
+    'OTHER',
 )
 
-# Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+t_OTHER     = r'.'
 
-# A regular expression rule with some action code
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)    
+def t_CMP(t):
+    r'(=|<>|<|>|<=|>=)'
+    t.type = 'CMP'
     return t
 
-# Define a rule so we can track line numbers
+def t_INTNUM(t):
+    r'\d+'
+    t.value = int(t.value)    
+    t.type = 'INTNUM'
+    return t
+
+def t_STRING(t):
+    r'[a-zA-Z0-9_-]+'
+    t.type = 'STRING'
+    return t
+
+t_ignore  = ' \t'
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
-
-# Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-
-
-
 
 if __name__ == '__main__':
     lexer = lex.lex()
